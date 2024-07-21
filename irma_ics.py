6814be4a3e154619b11fb06dbfc783ec9bcd6ed5
Pietari Hyvärinen <Kallio@users.noreply.github.com>
2024-07-21 20:17:06 +0300

    Update irma_ics.py
    complete rewrite during a sailing trip.

--- irma_ics.py
+++ irma_ics.py
@@ -1,157 +1,136 @@
-"""Convert irma calendar to caldav.
-
-Usage instructions::
-
-    python3 -m venv venv
-    source venv/bin/activate
-    pip install requests ics
-    LOGLEVEL=DEBUG python irma_ics.py ilmodedis
-or
-    LOGLEVEL=DEBUG python irma_ics.py events
-
-
-"""
-
-import re
-import os
-from pathlib import Path
+import json
+import os 
+import hashlib
 from datetime import datetime, timedelta
-import sys
-import argparse
+import arrow.parser
 import logging
+import pytz
+
+from pathlib import Path
+# import argparse
 import urllib
 import requests
-import arrow.parser
 from ics import Calendar, Event, DisplayAlarm
-
 LOGGER = logging.getLogger("irma-ics")
 logging.basicConfig(level=os.environ.get("LOGLEVEL", logging.WARNING))
 
-BASE_URL = "https://irma.suunnistusliitto.fi"
 
-CALENDAR_URL = (
-    f"{BASE_URL}/irma/public/competitioncalendar"
-    "/view?dicsipline=SUUNNISTUS&competitionOpen=ALL"
-)
-
-EVENT_KEYS = {
-    "1. ilmoittautumisporras": "emit_registration_date",
-    "Kilpailupäivä": "event_name",
-    "Päivämäärä": "event_date",
-    "taso": "event_level",
-    "WWW": "competition_www",
-}
-
-DATE_FORMATS = [
-    "%d.%m.%Y",
-    "%d.%m.%Y %H:%M:%S",
-]
-
+# Create a new calendar
+calendar = Calendar()
+ilmodediscalendar = Calendar()
+base_url = "https://irma.suunnistusliitto.fi/public/competition/view/"       
+range_start = datetime.now()
+filecache_expiry = range_start+timedelta(days=2)
+range_end = range_start+timedelta(days=36)
 
 def get_cache(identifier):
     """magic cache"""
-    filename = re.sub(r"[^0-9a-zA-Z]+", "_", identifier)
-    filename = re.sub(r"jsessionid_[A-Z0-9]+_", "_", filename)
+    filename = identifier
     cache = Path(f"cache/{filename}")
     cache.parent.mkdir(exist_ok=True)
     return cache
 
-
-def get_url(url):
+def get_url(url,cookies,heades,json_data,identifier):
     """Return page from local cache"""
-
-    cache = get_cache(url)
-    if cache.exists():
+    """ create unique cache identifier for url """
+    hs = hashlib.sha256((url+identifier).encode('utf-8')).hexdigest()
+    cache = get_cache(hs)
+    if cache.exists() and (datetime.fromtimestamp(cache.stat().st_mtime).isoformat() <= filecache_expiry.isoformat()) :
         LOGGER.debug("Cached %s", cache.name)
         return cache.read_text("utf8")
+   
+    response = requests.post(url,
+    cookies=cookies,
+    headers=headers,
+    json=json_data,
+    )
+    if response.status_code != 200:
+        raise Exception(f"Failed to fetch data from {url}")
 
-    response = requests.get(url)
     cache.write_text(response.text, "utf8")
     return response.text
 
+cookies = {
+    'JSESSIONID': '4CBF14F076D63FA6C56DABD308589315',
+    'csrfToken': 'd5d9fb69-bd73-447a-9351-5a0e5ed0586f',
+    'locale': 'fi',
+}
 
-def parse_event_urls(html):
-    """return event info urls from event calendar"""
-    for line in html.splitlines():
-        match = re.match('.*(/irma/public/competition/view[^"]+)', line)
-        if match:
-            yield match.group(1)
+headers = {
+  #  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0',
+    'Accept': 'application/json',
+    'Accept-Language': 'en-US,en;q=0.5',
+    # 'Accept-Encoding': 'gzip, deflate, br, zstd',
+   # 'Referer': 'https://irma.suunnistusliitto.fi/public/competitioncalendar/list',
+    'Content-Type': 'application/json',
+    'X-CSRF-Token': 'd5d9fb69-bd73-447a-9351-5a0e5ed0586f',
+    'Origin': 'https://irma.suunnistusliitto.fi',
+    #'Connection': 'keep-alive',
+    # 'Cookie': 'JSESSIONID=4CBF14F076D63FA6C56DABD308589316; csrfToken=d5d9fb69-bd73-447a-9351-5a0e5ed0586f; locale=fi',
+    #'Sec-Fetch-Dest': 'empty',
+    #'Sec-Fetch-Mode': 'cors',
+    #'Sec-Fetch-Site': 'same-origin',
+    'Priority': 'u=0',
+}
 
+front_page_json_data = {
+    'year': None, # can be 2028,2027,2026,2025,2024,2023,2022,2021,2020,2019,2018,2017,2016,2015,2014,2013,2012,2011,2010,2007,2000,2
+    'discipline': 'SUUNNISTUS', # can be also "HIIHTO","PYORAILY","TARKKUUS"
+    'areaId': None,
+    'calendarType': 'all',
+    'competitionOpen': 'ALL',
+}
 
-def parse_rows(html):
-    """Parse table rows / columns from html"""
-    for row in re.sub(r"\s+", " ", html).split("(.*?)= range_start.isoformat()) and (iso_date(event_info["event_date"]) <= range_end.isoformat()):
-            ics = Event()
-            ics.name = f" {event_name}"
-            ics.begin = iso_date(event_info["event_date"])
-            ics.url =event_info["url"]
-            ics.make_all_day()
+#{areaId can be {"id":1,"name":"Etelä-Pohjanmaa","abbreviation":"E-P"},{"id":2,"name":"FSO","abbreviation":"FSO"},{"id":3,"name":"Häme","abbreviation":"Häme"},{"id":6,"name":"Kaakko","abbreviation":"Kaak"},{"id":7,"name":"Kainuu","abbreviation":"Kain"},{"id":4,"name":"Keski-Pohjanmaa","abbreviation":"K-P"},{"id":5,"name":"Keski-Suomi","abbreviation":"K-S"},{"id":9,"name":"Lappi","abbreviation":"Lap"},{"id":10,"name":"Pohjois-Pohjanmaa","abbreviation":"P-P"},{"id":8,"name":"Päijät-Häme","abbreviation":"P-H"},{"id":12,"name":"Satakunta","abbreviation":"Sata"},{"id":11,"name":"Savo-Karjala","abbreviation":"S-K"},{"id":13,"name":"Uusimaa","abbreviation":"Uusi"},{"id":14,"name":"Varsinais-Suomi","abbreviation":"V-S"}]
 
-    # Lisää hälytys 1 päivä ennen emit_registration_date:tä, jos se on saatavilla
-            if "emit_registration_date" in event_info:
-                ics.alarm = event_info["emit_registration_date"]
+f = get_url('https://irma.suunnistusliitto.fi/connect/CompetitionCalendarEndpoint/view',cookies,headers,front_page_json_data,"0")
+data = json.loads(f)
 
-            yield ics
-    except (ValueError, arrow.parser.ParserError) as exception:
-        LOGGER.warning("create_ics_events:raceday:%s:%s", event_name, str(exception))
-
-def create_ics_ilmodedis(event_info):
-    """Return event ilmodedis from calendar """
-    event_name = normalize_event_name(event_info)
-    range_start = datetime.now()
-    range_end = range_start +timedelta(days=365)
-
+# Parse each event and add to the calendar
+for item in data:
+    
     try:
-        if (iso_date(event_info["emit_registration_date"]) >= range_start.isoformat()) and (iso_date(event_info["emit_registration_date"]) <= range_end.isoformat()):
-            ics = Event()
-            ics.name = f"ilmodedis: {event_name}"
-            ics.begin = iso_date(event_info["emit_registration_date"])
-            ics.url =event_info["url"]
-            ics.make_all_day()
-            yield ics
+        if ((item['competitionDate']) >= range_start.isoformat()) and ((item['competitionDate']) <= range_end.isoformat()):
+            event = Event()
+            event.name = item['competitionDayName']
+            event.begin =datetime.strptime(item['competitionDate'], "%Y-%m-%dT%H:%M:%S.%f%z")+timedelta(hours=3)
+            event.status = "CONFIRMED"  
+            competition_id = item['dayId']
+            event.url =f"{base_url}{competition_id}"
+            event.make_all_day()
+            event.description = f"Järjestävät seurat: {', '.join(club['name'] for club in item['organisingClubs'])}"
+            event.categories = [item['sport']]
+            print(str(event.begin) + event.name)
+            calendar.events.add(event)
+            
+            if item['registrationAllowed'] is True:
+                ilmodedis = Event()
+                ilmodedis.begin = datetime.strptime(item['competitionDate'], "%Y-%m-%dT%H:%M:%S.%f%z")+timedelta(days=-10)+timedelta(hours=3)
+                ilmodedis.name = "#Ilmodedis "+item['competitionDayName']
+                ilmodedis.status = "TENTATIVE"
+                print(ilmodedis.name)
+                suburl= 'https://irma.suunnistusliitto.fi/connect/CompetitionEndpoint/viewCompetitionDay'
+                competition_id = item['dayId']
+                ilmodedis.url=f"{base_url}{competition_id}"
+                ilmodedis.make_all_day()
+                ilmodedis.categories = [item['sport']]
+                json_data = { 'id': competition_id,}
+                ff = get_url(suburl, cookies,headers,json_data,str(competition_id),)
+                competitiondata = json.loads(ff)
+                ilmodedis.begin = datetime.strptime(competitiondata['registrationPeriod']['firstRegistrationPeriodClosingDate'], "%Y-%m-%dT%H:%M:%S.%f%z")+timedelta(hours=3)
+                ilmodedis.name = "Ilmodedis "+item['competitionDayName']
+                ilmodedis.status = "TENTATIVE"
+                ilmodedis.make_all_day()
+                print(str(ilmodedis.begin) + ilmodedis.name)
+                ilmodediscalendar.events.add(ilmodedis)
+                               
     except (ValueError, arrow.parser.ParserError) as exception:
-        LOGGER.warning("create_ics_events:emit_date:%s:%s", event_name, str(exception))
-
-
-def event_key(event):
-    """unique key for removing duplicates"""
-    return str(event.begin) + str(event.name)
-
-def main():
-    """print ics calendar from irma events"""
-
-
-    parser=argparse.ArgumentParser(description="choose ilmodedis or events")
-    parser.add_argument("type")
-    args=parser.parse_args()
-
-
-    main_page = get_url(CALENDAR_URL)
-
-    events = {}
-
-    for event_url in parse_event_urls(main_page):
-        event_page = get_url(BASE_URL + event_url)
-        event_info = parse_event_info(event_page)
-         # Lisätään tapahtuman URL sen tietoihin
-        event_info["url"] =BASE_URL +'/irma/public/competition/view?' +urllib.parse.urlparse(BASE_URL+event_url).query
-        if args.type=="ilmodedis":
-            for event in create_ics_ilmodedis(event_info):
-                    events[event_key(event)] = event
-
-        else :
-            for event in create_ics_events(event_info):
-                    events[event_key(event)] = event
-
-
-    calendar = Calendar()
-
-    for event in events.values():
-        calendar.events.add(event)
-
-
-    print("".join(calendar.serialize_iter()))
-
-
-if __name__ == "__main__":
-    main()
+        LOGGER.warning("create_ics_error:%s:%s", event_name, str(exception))
+   
+# Write the calendar to an .ics file
+with open('irma_events.ics', 'w', encoding='utf-8') as f:
+    print(" generating competitioninfo")
+    f.writelines(calendar)
+with open('irma_ilmodedis.ics', 'w', encoding='utf-8') as fd:
+    print(" generating ilmodedis")
+    fd.writelines(ilmodediscalendar)
